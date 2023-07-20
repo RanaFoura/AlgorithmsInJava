acc51f1c078f009ab0dc15e7e3bd0d4c79d022a5
RanaFoura <115618347+RanaFoura@users.noreply.github.com>
2023-07-20 13:42:29 +0300

    Selection Sort , Insertion Sort , Bubble Sort

--- src/Algo.java
+++ src/Algo.java
@@ -1,24 +1,77 @@
 public class Algo {
 
 
+        // O(n^2)
+    public static void sortSelection(int arr[]){
+        for (int i =0 ; i < arr.length; i++ ){
+            int currentminimum = arr[i];
+            int currentitem = i;
+            for (int j = i ; j < arr.length;j++) {
+                if (arr[j]<currentminimum){
+                    currentminimum=arr[j];
+                    currentitem=j;
+                }
+            }
+            // Swap the minimum element with the first unsorted element
+            arr[currentitem] = arr[i];
+            arr[i]=currentminimum;
+        }
+        // Print the sorted array
+        for (int i =0 ; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+
     // O(n^2)
-public static void selectionSort(int arr[]){
-    for (int i =0 ; i < arr.length; i++ ){
-        int currentminimum = arr[i];
-        int currentitem = i;
-        for (int j = i ; j < arr.length;j++) {
-            if (arr[j]<currentminimum){
-                currentminimum=arr[j];
-                currentitem=j;
+    public static void sortBubble(int arr[]){
+        int unsorted= arr.length;
+        boolean swapped;
+        for (int i =0 ; i < arr.length; i++ ){
+            swapped= false;
+            for (int j=0; j<unsorted;j++){
+                if (j+1 <=arr.length-1) {
+                    if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            // This will make O(n) instead of o(n^2), if the array sorted , or just stop looping if the array sorted
+            if (swapped == false)
+                break;
+            unsorted--;
+        }
+        // Print the sorted array
+        for (int i =0 ; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+
+    // O(n^2)
+    public static void sortInsertion(int arr[]){
+    for (int i = 0 ; i<arr.length-1;i++){
+        for ( int j = i+1 ; j>0 ; j--){
+            if (arr[j]>arr[j-1]){
+                break;
+            }
+            else {
+                int temp=arr[j-1];
+                arr[j-1]=arr[j];
+                arr[j]= temp;
             }
         }
-        // Swap the minimum element with the first unsorted element
-        arr[currentitem] = arr[i];
-        arr[i]=currentminimum;
     }
-    // Print the sorted array
-    for (int i =0 ; i< arr.length;i++){
-        System.out.println(arr[i]);
+        // Print the sorted array
+        for (int i =0 ; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
     }
+
 }
-}
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int arr3[] = {1,2,3,4,5,6,7,8,9,10}; //sorted
         int arr[] = {64,25,12,22,51};
+        int arr2[] = {2,8,5,3,9,4,1};
+        int[] ascendingArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] descendingArray = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] arrayWithDuplicates = {5, 2, 8, 2, 1, 5, 7, 8, 3, 4};
+        int[] shuffledArray = {7, 3, 9, 2, 6, 1, 8, 4, 5, 10};
+        int[] negativeArray = {-5, -3, -8, -2, -6, -1, -7, -4, -9, -10};
+        int[] singleElementArray = {42};
+        int[] emptyArray = {};
+        int[] largeArray = {100, 45, 87, 34, 78, 56, 23, 99, 101, 66, 90, 72, 42, 29, 37, 91, 88, 55, 13, 24};
 
-        Algo.selectionSort(arr);
+
+//        Algo.sortSelection(arr2);
+//        Algo.sortBubble(arr2);
+        Algo.sortInsertion(arr2);
 
 
     }
